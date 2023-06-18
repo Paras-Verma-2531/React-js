05cfdb0f6e44033befea8071d825060c2af8c839
paras <parasverma2531@gmail.com>
2023-06-19 03:39:56 +0530

    react info site project

--- react_learning/src/App.js
+++ react_learning/src/App.js
@@ -1,16 +1,27 @@
 import "./App.css";
-import Header from './components/Header'
-// here Page is a reatc reusable comp:
+import Nav from './components/Nav'
+import Main from './components/Main'
+// import Header from './components/Header'
 export default function App() {
   return (
-    <div>
-      <Header />
-      <h1>Welcome to my Page</h1>
-      <ul>
-        <li>I love Cricket</li>
-        <li>I love Music</li>
-        <li>I am a big foodie</li>
-      </ul>
+    <div className="container">
+      <Nav/>
+      <Main/>
     </div>
   );
-}
+};
+// here Page is a reatc reusable comp:
+// export default function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <h1>Welcome to my Page</h1>
+//       <ul>
+//         <li>I love Cricket</li>
+//         <li>I love Music</li>
+//         <li>I am a big foodie</li>
+//       </ul>
+//     </div>
+//   );
+// }
+// for the react site info project:
